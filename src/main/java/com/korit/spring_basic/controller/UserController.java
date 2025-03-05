57c0f01a4a76f66c71f6d18ab2ee5a258e41fa01
young <lovetkddud@naver.com>
2025-03-05 17:29:45 +0900

    Update UserController.java

--- src/main/java/com/korit/spring_basic/controller/UserController.java
+++ src/main/java/com/korit/spring_basic/controller/UserController.java
@@ -1,11 +1,15 @@
 package com.korit.spring_basic.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.korit.spring_basic.dto.GetUserResponseDto;
 import com.korit.spring_basic.dto.PostUserRequestDto;
 import com.korit.spring_basic.dto.ResponseDto;
 import com.korit.spring_basic.service.UserService;
@@ -22,6 +26,19 @@ public class UserController {
     @PostMapping("")
     public ResponseEntity<ResponseDto> postUser(@RequestBody @Valid PostUserRequestDto requestBody) {
         ResponseEntity<ResponseDto> response = userService.postUser(requestBody);
+        return response;
+    }
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<? super GetUserResponseDto> getUser(@PathVariable("userId") String userId) {
+        ResponseEntity<? super GetUserResponseDto> response = userService.getUser(userId);
+        return response;
+    }
+
+    @DeleteMapping("/{userId}")
+    public ResponseEntity<ResponseDto> deleteUser(@PathVariable("userId") String userId) {
+
+        ResponseEntity<ResponseDto> response = userService.deleteUser(userId);
         return response;
     }
 }
