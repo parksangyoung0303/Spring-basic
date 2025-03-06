2b07eeb6d274dbcf1c6196ce8c096dc44c0c5d70
young <lovetkddud@naver.com>
2025-03-06 17:42:36 +0900

    Update UserService.java

--- src/main/java/com/korit/spring_basic/service/UserService.java
+++ src/main/java/com/korit/spring_basic/service/UserService.java
@@ -2,7 +2,9 @@ package com.korit.spring_basic.service;
 
 import org.springframework.http.ResponseEntity;
 
+import com.korit.spring_basic.dto.GetUserListResponseDto;
 import com.korit.spring_basic.dto.GetUserResponseDto;
+import com.korit.spring_basic.dto.PatchUserRequestDto;
 import com.korit.spring_basic.dto.PostUserRequestDto;
 import com.korit.spring_basic.dto.ResponseDto;
 
@@ -10,7 +12,12 @@ public interface UserService {
     
     ResponseEntity<ResponseDto> postUser(PostUserRequestDto dto);
 
+    ResponseEntity<? super GetUserListResponseDto> getUserList();
+
     ResponseEntity<? super GetUserResponseDto> getUser(String userId);
 
+    ResponseEntity<ResponseDto> patchUser(String userId, PatchUserRequestDto dto);
+
     ResponseEntity<ResponseDto> deleteUser(String userId);
+
 }
