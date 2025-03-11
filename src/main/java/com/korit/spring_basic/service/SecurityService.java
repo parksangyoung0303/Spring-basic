20cb5beca3671d41b2d8a5a0945cdf0b8a84827f
young <lovetkddud@naver.com>
2025-03-11 17:19:05 +0900

    Update SecurityService.java

--- src/main/java/com/korit/spring_basic/service/SecurityService.java
+++ src/main/java/com/korit/spring_basic/service/SecurityService.java
@@ -2,8 +2,10 @@ package com.korit.spring_basic.service;
 
 import org.springframework.http.ResponseEntity;
 
+import com.korit.spring_basic.dto.SignInRequestDto;
 import com.korit.spring_basic.dto.SignUpRequestDto;
 
 public interface SecurityService {
   ResponseEntity<String> signUp(SignUpRequestDto dto);
+  ResponseEntity<String> signIn(SignInRequestDto dto);
 }
